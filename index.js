271055bb8e5360ae6391da9c8a532d0283449c34
7toR <kostya5270@gmail.com>
2024-06-10 11:16:35 +0200

    added function to change block color

---
+++ index.js
@@ -0,0 +1,9 @@
+out1 = document.querySelector(`.out-1`)
+console.log(out1);
+
+document.querySelector(`.b-1`).addEventListener(`click`, () => {
+  out1.classList.toggle("inPurple")
+  console.log(123);
+})
+
+console.log(1);
