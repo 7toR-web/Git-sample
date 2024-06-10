77909ac296cedc8f304ac96f3502ff78ee1161bb
7toR <kostya5270@gmail.com>
2024-06-10 13:47:05 +0200

    obj clone by using JSON

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ document.querySelector(`.b-1`).addEventListener(`click`, () => {
 })
 
 console.log(1);
+
+const obj = {
+  name: `Kostya`,
+  age: 17,
+  isMaried: false,
+  family: {
+    mom: true,
+    dad:true,
+    sister: true,
+    brother: false
+  },
+  food: `sushi`
+}
+
+const clone = JSON.parse(JSON.stringify(obj))
+console.log(obj)
+console.log(clone)
+console.log(obj == clone);
